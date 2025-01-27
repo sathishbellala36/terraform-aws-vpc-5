c6d1f308be18dbe2989c4f46b173af1deff29b07
sathishbellala36 <sathishbellala36@gmail.com>
2025-01-27 15:49:13 +0530

    user-profile

---
+++ outputs.tf
@@ -0,0 +1,25 @@
+output "vpc_id" {
+    value = aws_vpc.main.id
+}
+output "public_subnet_ids" {
+    value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+    value = aws_subnet.private[*].id
+}
+
+output "databse_subnet_ids" {
+    value = aws_subnet.database[*].id
+}
+# output "az_info" {
+#     value = data.aws_availability_zones.available
+# }
+
+# output "default_vpc_info" {
+#     value = data.aws_vpc.default
+# }
+
+# output "main_route_table_info" {
+#     value = data.aws_route_table.main
+# }
